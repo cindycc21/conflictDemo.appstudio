5c360721f02f2231dabfd429401013027b89daf6
Dr. Cindy Corritore <CynthiaCorritore@creighton.edu>
2021-04-17 17:59:59 -0500

    firstset

--- forms/GradeSwitch/GradeSwitch.js
+++ forms/GradeSwitch/GradeSwitch.js
@@ -1,31 +1,29 @@
-// User's letter grade
+Button1.onclick = function() {
+  // User's letter grade
 
-
-Button1.onclick=function(){
   let grade = prompt("Enter your letter grade using a capital letter.")
-let name = prompt("Enter your first name: ")
+  let name = prompt("Enter your first name: ")
 
-// Give user feedback on grade
+  // Give user feedback on grade
 
-switch (grade) {
-case "A":
-  alert`${name} Good Job`)
-  break
-case "B":
-  alert("Pretty Good Job")
-  break
-case "C":
-  alert("Passed")
-  break
-case "D":
-  alert("Not so good")
-  break;
-case "F":
-  alert("Whoops, failed")
-  break
-default:
-  alert("Uh oh")
-  break
-}
-
+  switch (grade) {
+    case "A":
+    alert`${name} Good Job`)
+    break
+  case "B":
+    alert("Pretty Good Job")
+    break
+  case "C":
+    alert("Passed")
+    break
+  case "D":
+    alert("Not so good")
+    break;
+  case "F":
+    alert("Whoops, failed")
+    break
+  default:
+    alert("Uh oh")
+    break
+  }
 }
